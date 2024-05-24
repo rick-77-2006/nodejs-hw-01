9575d05aa11d73e74a0728061f9a4cc90bcb8905
Rick <46992687+rick-77-2006@users.noreply.github.com>
2024-05-24 21:42:04 +0300

    Add files via upload

---
+++ src/scripts/addOneContact.js
@@ -0,0 +1,20 @@
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf8');
+
+        const contacts = JSON.parse(data);
+        const newContact = createFakeContact();
+
+        contacts.push(newContact);
+
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+await addOneContact();
---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const generateContacts = async (number) => {
+    try {
+        let data = await fs.readFile(PATH_DB, 'utf8');
+
+        const contacts = JSON.parse(data);
+
+        for (let i = 0; i < number; i++) {
+            contacts.push(createFakeContact());
+        }
+
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+await generateContacts(5);
